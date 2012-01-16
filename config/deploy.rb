f3782fbbe1df6bc2d4084d89a2d351f80fe09516
Robert Glaser <mail@robert-glaser.de>
2012-01-17 00:09:24 +0100

    Bundler options for deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ ssh_opts = '-A'
 branch = 'inploy'
 user = 'iqvoc'
 login_shell = true
-bundler_opts = '--deployment --quiet --binstubs --without development test heroku'
+bundler_opts = '--deployment --without development test heroku'
 
 # before_restarting_server do
 #   rake "important:task"
